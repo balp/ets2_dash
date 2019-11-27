5529babadc724a42c958d11d30deadfc4db6fda0
Anders Arnholm <anders@arnholm.se>
2019-11-27 06:22:23 +0100

    Multithreading, as PysimpleGUI is simpler
    PySimpleGUI no longer have a non-blocking read to be
    placed inside mqtt's client loop. So now we need one
    mqtt and one gui thread.

--- ets2_dash/gui.py
+++ ets2_dash/gui.py
@@ -3,18 +3,23 @@ import paho.mqtt.client as mqtt
 import PySimpleGUI
 import json
 import typing
-import decimal
+import logging
+import threading
+from dataclasses import dataclass
 
 from ets2_dash.model import Model
 from ets2_dash.view import View
 
+@dataclass
+class GlobalState:
+    active: bool = True
 
-model: Model = Model()
 
-
-def on_message(client : typing.Any, userdata : typing.Any , message : object):
+def on_message(client: typing.Any, userdata: typing.Any , message : object):
     # print("message received ", message.payload)
     # print("message topic=",message.topic)
+    assert isinstance(userdata, Model)
+    model = userdata
     json_data = json.loads(message.payload)
     if message.topic == "ets2/data":
         model.set_telematic_data(json_data)
@@ -26,41 +31,40 @@ def on_message(client : typing.Any, userdata : typing.Any , message : object):
         model.set_job_config(json_data)
 
 
-
-def main():
-    print("Startup!!!")
-
-    client = mqtt.Client("ets2_gui") #create new instance
+def mqtt_thread_loop(model: Model, state: GlobalState):
+    client = mqtt.Client("ets2_gui", userdata=model) #create new instance
     client.on_message = on_message
     client.connect("localhost") #connect to broker
     client.subscribe("ets2/data")
     client.subscribe("ets2/game")
     client.subscribe("ets2/info")
     client.subscribe("ets2/info/config/job")
-
     # client.subscribe("ets2/info/config/substances")
     # client.subscribe("ets2/info/config/controls")
     # client.subscribe("ets2/info/config/hshifter")
     # client.subscribe("ets2/info/config/truck")
     # client.subscribe("ets2/info/config/trailer")
+    while state.active:
+        client.loop(timeout=1.0)
+
+def main():
+    print("Startup!!!")
+    model: Model = Model()
+    state: GlobalState = GlobalState()
+    mqtt_reader_thread = threading.Thread(target=mqtt_thread_loop, args=(model, state))
+    mqtt_reader_thread.start()
 
     PySimpleGUI.ChangeLookAndFeel('Dark')
     hmi = View(model)
 
     print("Loop!!!")
     while (True):
-        # This is the code that reads and updates your window
-        button, values = hmi.window.Read(timeout=100)
-        # print("Loop data", myData.data)
+        event, values = hmi.window.Read(timeout=50)
         hmi.update_data()
-        if button == 'Exit':
+        if event == 'Exit':
             break
-
-        # Your code begins here
-        client.loop(timeout=0.01, max_packets=1)
-
-
-    # Broke out of main loop. Close the window.
+    state.active = False
+    mqtt_reader_thread.join(timeout=2.0)
     hmi.window.CloseNonBlockingForm()
 
 
